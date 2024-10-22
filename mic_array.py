84b8a5f9494c8b6649fa841647a6c9d6b4cfe4d6
Gl0dny <krystian.glodek1717@gmail.com>
2024-10-22 14:52:39 +0200

    fix vad and doa

--- mic_array.py
+++ mic_array.py
@@ -1,7 +1,7 @@
 
 
 import pyaudio
-import Queue
+import queue
 import threading
 import numpy as np
 from gcc_phat import gcc_phat
@@ -22,7 +22,7 @@ class MicArray(object):
 
     def __init__(self, rate=16000, channels=8, chunk_size=None):
         self.pyaudio_instance = pyaudio.PyAudio()
-        self.queue = Queue.Queue()
+        self.queue = queue.Queue()
         self.quit_event = threading.Event()
         self.channels = channels
         self.sample_rate = rate
